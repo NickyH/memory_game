449b3c525b36ac5fdfef8d4d6b808eed972f90e7
NickyH <nickyhughes00@gmail.com>
2013-05-12 19:49:43 -0400

    Updated remove select function

--- memory.js
+++ memory.js
@@ -48,8 +48,11 @@ function validate_table()
 
 function createTable()
 {
+  clicks = 0
   $('.headspace').slideUp();
+  $('#game').slideDown();
   $('#found').removeClass('hide').slideDown();
+  $('#found').empty();
   $('#clicks').removeClass('hide').slideDown();
   $('#reset').removeClass('hide');
   $('#hint').removeClass('hide');
@@ -64,12 +67,11 @@ function createTable()
   for( var row = 0; row < num_rows; row++ )
   {
     var new_row = document.createElement("tr");
-    new_row.className = "tr"+row;
+    // new_row.className = "tr"+row;
     new_table.appendChild(new_row);
     for( var col = 0; col < num_cols; col++ )
     {
     var new_col = document.createElement("td");
-    // new_col.className = "td"+col;
     var flip_container = $("<div></div>").attr({'class': 'flip-container'}).appendTo($(new_col));
     new_col.setAttribute('id', img_index + col);
     var div_flipper = $("<div></div>").attr({'class': 'flipper'}).appendTo($(flip_container));
@@ -81,7 +83,6 @@ function createTable()
     new_row.appendChild(new_col);
     }
     img_index = parseInt(img_index) + parseInt(num_cols);
-    console.log("img_index=" + img_index);
     }
   table_space.appendChild(new_table);
 }
@@ -100,7 +101,6 @@ function get_images()
       current_images[i] = current_images[j];
       current_images[j] = temp;
     }
-    console.log(current_images);
     return current_images;
 }
 
@@ -161,7 +161,6 @@ function select_image(cell)
       var found_img = file_name;
       $('#found').append(found_img);
       var selected_tds = document.getElementsByClassName('selected');
-      console.log(selected_tds.length);
       if (selected_tds.length === ((total_needed -1) * 2))
       {
         end_game();
@@ -184,8 +183,11 @@ function flip_all_images()
    $(this).removeClass('flipped');
 });
   var add_onclick = document.getElementsByTagName('td');
-  $(add_onclick).each(function() {
-   this.setAttribute('onclick', 'select_image(this)');
+    $(add_onclick).each(function() {
+      if ($(this).attr('class') !== 'selected')
+  {
+    this.setAttribute('onclick', 'select_image(this)');
+  }
 });
 }
 
@@ -249,8 +251,8 @@ function flash_cells(td1, td2)
 function end_game()
 {
   show_all();
-  $('.headspace').delay(5000).slideDown();
-  $('#game').delay(5000).slideUp();
+  $('.headspace').delay(4000).slideDown();
+  $('#game').delay(4000).slideUp();
   $('#rows').val('').attr('placeholder', "Rows");
   $('#cols').val('').attr('placeholder', "Columns");
   end_message();
@@ -258,10 +260,10 @@ function end_game()
 
 function end_message()
 {
-  console.log('message');
   $('#congrats').removeClass('hide');
+  $('#congrats').text('Nice work...That only took you ' + clicks + ' clicks');
   $('#congrats').delay(3000).queue(function(next){
-    $('#congrats').fadeOut(5000).addClass('hide');
+    $('#congrats').fadeOut(3000).addClass('hide');
     next();
     });
 }
